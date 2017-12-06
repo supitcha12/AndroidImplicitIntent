4d234075842245e60e7dbc74c3241ba158c25a18
supitcha1 <supitcha6014@gmail.com>
2017-12-06 16:34:45 +0700

    finish

--- app/src/main/java/com/example/navadroid/androidimplicitintent/MainActivity.java
+++ app/src/main/java/com/example/navadroid/androidimplicitintent/MainActivity.java
@@ -38,16 +38,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.btn_intent_2:
-                // TODO: ???
+                // TODO: call
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0882319890"));
+                startActivity(intent);
                 break;
             case R.id.btn_intent_3:
-                // TODO: ???
+                // TODO: web
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://facebook.com/kyuuuyk"));
+                startActivity(intent);
                 break;
             case R.id.btn_intent_4:
-                // TODO: ???
+                // TODO: open camera
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
                 break;
             case R.id.btn_intent_5:
                 // TODO: ???
+                intent.setAction(Intent.ACTION_VOICE_COMMAND);
+                startActivity(intent);
                 break;
 
         }
